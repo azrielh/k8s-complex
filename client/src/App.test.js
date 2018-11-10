47e9f6da72eb94822bced91e1839f5176c5d4f04
Az Hopkins <azhopkins@gmail.com>
2018-11-09 16:30:57 -0800

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+it("renders without crashing", () => {
+  // does not work as it is making an external request in fib.js
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
