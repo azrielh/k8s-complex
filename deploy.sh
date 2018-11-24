60681cdd9a04eb41b04ff0dd86a8acf409ba1caa
Az Hopkins <azhopkins@gmail.com>
2018-11-24 10:59:43 -0800

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t azrielh/complex-client:latest -t azrielh/complex-client:$SHA -f ./client/Dockerfile ./client
 docker build -t azrielh/complex-server:latest -t azrielh/complex-server:$SHA -f ./server/Dockerfile ./server
 docker build -t azrielh/complex-worker:latest -t azrielh/complex-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push azrielh/complex-client:latest
 docker push azrielh/complex-server:latest
 docker push azrielh/complex-worker:latest
@@ -10,7 +11,6 @@ docker push azrielh/complex-server:$SHA
 docker push azrielh/complex-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=azrielh/complex-client:$SHA
 kubectl set image deployments/server-deployment server=azrielh/complex-server:$SHA
+kubectl set image deployments/client-deployment client=azrielh/complex-client:$SHA
 kubectl set image deployments/worker-deployment worker=azrielh/complex-worker:$SHA
-
